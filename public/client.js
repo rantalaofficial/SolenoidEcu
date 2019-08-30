f62389d5f9e00399938e186bbcb2901840a39562
UisDangerouz <33716618+UisDangerouz@users.noreply.github.com>
2019-08-30 23:39:17 +0300

    Version 1.0

---
+++ public/client.js
@@ -0,0 +1,210 @@
+const socket = io.connect('/');
+
+let inputs;
+let outputs;
+
+let rpmGauge;
+let rpmChart;
+let rpmLine;
+let rpmTargetLine;
+
+$(document).ready(() => {
+    //PAGE ELEMENTS
+    rpmGauge = new JustGage({
+        id: 'rpmGauge',
+        value: 0,
+        min: 0,
+        max: 1200,
+        label: 'RPM',
+        labelFontColor: 'black',
+        levelColorsGradient: true,
+        levelColors: ['#00ff00', '#fffa00', '#ff0000'],
+        gaugeColor: '#bebebe',
+        relativeGaugeSize: true,
+    });
+    initRPMChart();
+    
+    //ENGINE INPUTS
+    //GENERAL
+    $('#ignitionBtn').click(() => {
+        if(inputs.ignition) {
+            inputs.ignition = false;
+        } else {
+            inputs.ignition = true;
+        }
+        socket.emit('SET', inputs);
+    });
+    $('#stroke2Btn').click(() => {
+        inputs._2stroke = true;
+        socket.emit('SET', inputs);
+    });
+    $('#stroke4Btn').click(() => {
+        inputs._2stroke = false;
+        socket.emit('SET', inputs);
+    });
+
+    //RAW
+    $('#rawControlBtn').click(() => {
+        inputs.raw = true;
+        socket.emit('SET', inputs);
+    });
+    $('#firingDelayInput').on('input', () => {
+        inputs.firingDelay = parseInt($('#firingDelayInput').val());
+        socket.emit('SET', inputs);
+    });
+    $('#firingDurationInput').on('input', () => {
+        inputs.firingDuration = parseInt($('#firingDurationInput').val());
+        socket.emit('SET', inputs);
+    });
+    //PID
+    $('#pidControlBtn').click(() => {
+        inputs.raw = false;
+        socket.emit('SET', inputs);
+    });
+    $('#rpmTargetInput').on('input', () => {
+        inputs.targetRPM = parseInt($('#rpmTargetInput').val());
+        socket.emit('SET', inputs);
+    });
+
+
+});
+
+function initRPMChart() {
+    let rpmChartElement = document.getElementById("rpmChart");
+    let rpmChartCtx = rpmChartElement.getContext('2d');
+
+    if($(window).width() < 800) {
+        rpmChartCtx.canvas.width = 150;
+    } else {
+        rpmChartCtx.canvas.width = 250;
+    }
+
+    rpmChart = new SmoothieChart({grid: {fillStyle: '#e2e2e2', sharpLines: true, verticalSections: 4}, labels: {fillStyle: '#000000', precision:0, fontSize: 16}, tooltip: true, minValue: 0, maxValue: 1000});
+    rpmChart.streamTo(rpmChartElement, 100);
+
+    rpmTargetLine = new TimeSeries();
+    rpmChart.addTimeSeries(rpmTargetLine, {lineWidth: 3, strokeStyle: 'yellow', fillStyle: 'rgba(255,255,166,0.30)'});
+
+    rpmLine = new TimeSeries();
+    rpmChart.addTimeSeries(rpmLine, {lineWidth: 3, strokeStyle: 'blue'});
+}
+
+//SOCKET HANDLERS
+socket.on('INPUTS', (data) => {
+    inputs = data;
+    updateInputs(inputs);
+});
+
+socket.on('OUTPUTS', (data) => {
+    outputs = data;
+    updateOutputs(outputs);
+});
+
+socket.on('LOG', (data) => {
+    updateLog(data);
+});
+
+socket.on('disconnect', () => {
+    $('#logDiv').append('<p>Connection lost.<p>');
+    $('#logDiv').scrollTop($('#logDiv')[0].scrollHeight - $('#logDiv').height());
+});
+
+
+function updateInputs(inputs) {
+    if(inputs._2stroke) {
+        $('#stroke2Btn').addClass('pressed');
+        $('#stroke4Btn').removeClass('pressed');
+    } else {
+        $('#stroke2Btn').removeClass('pressed');
+        $('#stroke4Btn').addClass('pressed');
+    }
+
+    if(inputs.raw) {
+        $('#rpmGauge').show();
+        $('#rpmChart').hide();
+
+        disableDiv('#rawControl', false);
+        disableDiv('#pidControl', true);
+
+        $('#rawControlBtn').addClass('pressed');
+        $('#pidControlBtn').removeClass('pressed');
+    } else {
+        $('#rpmGauge').hide();
+        $('#rpmChart').show();
+
+        disableDiv('#rawControl', true);
+        disableDiv('#pidControl', false);
+
+        $('#rawControlBtn').removeClass('pressed');
+        $('#pidControlBtn').addClass('pressed');
+    }
+
+    setBtnState('#ignitionBtn', inputs.ignition);
+
+    $('#firingDelayText').text(inputs.firingDelay + ' ms');
+    $('#firingDelayInput').val(inputs.firingDelay);
+
+    $('#firingDurationText').text(inputs.firingDuration + ' ms');
+    $('#firingDurationInput').val(inputs.firingDuration);
+
+    $('#rpmTargetText').text(inputs.targetRPM + ' RPM');
+    $('#rpmTargetInput').val(inputs.targetRPM);
+
+    $('#KpText').text(inputs.Kp)
+    $('#KiText').text(inputs.Ki)
+    $('#KdText').text(inputs.Kd)
+}
+
+function updateOutputs(outputs) {
+    if(inputs.raw) {
+        rpmGauge.refresh(outputs.rpm);
+    } else {
+        rpmTargetLine.append(new Date().getTime(), inputs.targetRPM)
+        rpmLine.append(new Date().getTime(), outputs.rpm)
+    }
+    
+    $('#sensor1Text').text(outputs.sensor[0]);
+    $('#sensor2Text').text(outputs.sensor[1]);
+
+    $('#solenoid1Text').text(outputs.solenoid[0]);
+    $('#solenoid2Text').text(outputs.solenoid[1]);
+}
+
+function disableDiv(id, state) {
+    if(state) {
+        $(id).children().css({'opacity' : 0.2})
+        $(id).children().prop('disabled', true);
+    } else {
+        $(id).children().css({'opacity' : 1})
+        $(id).children().prop('disabled', false);
+    }
+}
+
+function setBtnState(id, state) {
+    if(state) {
+        $(id).val('ON')
+        $(id).addClass('on');
+    } else {
+        $(id).val('OFF')
+        $(id).removeClass('on');
+    }
+}
+
+//HELPER FUNCTIONS
+function zeroFill(number, width) {
+    width -= number.toString().length;
+    if (width > 0) {
+        return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+    }
+    return number + "";
+}
+
+function updateLog(log) {
+    $('#logDiv').empty();
+
+    for(let i = 0; i < log.length; i++) {
+        $('#logDiv').append('<p><span>[' + log[i][0] + ']</span> ' + log[i][1] + '<p>');
+    }
+
+    $('#logDiv').scrollTop($('#logDiv')[0].scrollHeight - $('#logDiv').height());
+}
---
+++ server.js
@@ -0,0 +1,186 @@
+const express = require('express');
+const socket = require('socket.io');
+const Gpio = require('onoff').Gpio;
+const PidController = require('node-pid-controller');
+
+//APP SETUP
+const serverPort = 8080;
+const app = express();
+const server = app.listen(serverPort, () => {
+	logText("Server started on port: " + serverPort)
+});
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(express.static('public'));
+const io = socket(server);
+
+let socketsConnected = 0;
+
+let serverLog = [];
+
+//SENSORS
+let sensor = [];
+sensor[0] = new Gpio(22, 'in', 'both');
+sensor[1] = new Gpio(27, 'in', 'both');
+
+//SOLENOIDS
+let solenoid = []
+solenoid[0] = new Gpio(17, 'out');
+solenoid[1] = new Gpio(18, 'out');
+
+//RPM CALCULATION
+let lastReadings = [];
+
+process.on('SIGINT', () => {
+    console.log('Shutting down the server...')
+    sensor[0].unexport();
+    sensor[1].unexport();
+    solenoid[0].unexport();
+    solenoid[1].unexport();
+    process.exit(0);
+});
+
+const inputDefaults = {
+    targetRPM: 150,
+
+    firingDelay: 10,
+    firingDuration: 10,
+}
+
+let inputs = {
+    ignition: true,
+    _2stroke: true,
+    raw: true,
+
+    targetRPM: inputDefaults.targetRPM,
+
+    firingDelay: inputDefaults.firingDelay,
+    firingDuration: inputDefaults.firingDuration,
+    firingDurationMin: 1,
+    firingDurationMax: 100,
+
+    Kp: 0.25,
+    Ki: 0.01,
+    Kd: 0.01,
+}
+
+let outputs = {
+    sensor: [0, 0],
+    solenoid: [0, 0],
+    rpm: 0,
+}
+
+//RPM PID CONTROLLER
+function resetRpmController() {
+    return new PidController({
+        k_p: inputs.Kp,
+        k_i: inputs.Ki,
+        k_d: inputs.Kd,
+    });
+}
+let RpmController = resetRpmController();
+
+//ENGINE CONTROLS
+sensor[0].watch((err, value) => {
+    sensorStateChanged(0, err, value);
+});
+sensor[1].watch((err, value) => {
+    sensorStateChanged(1, err, value);
+});
+
+function sensorStateChanged(id, err, value) {
+    if (err) { throw err }; 
+    outputs.sensor[id] = value;
+    
+    if(value) {
+        lastReadings.push(Date.now());
+
+        if(!inputs.ignition) {return;}
+
+        setTimeout(() => {
+            changeSolenoidState(id, 1)
+            
+            setTimeout(() => {
+                changeSolenoidState(id, 0)
+            }, inputs.firingDuration);
+        }, inputs.firingDelay)
+    }
+}
+
+function changeSolenoidState(id, value) {
+    solenoid[id].writeSync(value)
+    outputs.solenoid[id] = value;
+}
+
+//ECU MAIN LOOP
+setInterval(() => {
+    //CALCULATE RPM
+    for(let i = lastReadings.length; i >= 0; i--) {
+        if(lastReadings[i] < Date.now() - 1100) {
+            lastReadings.splice(i, 1);
+        }
+    }
+    outputs.rpm = ((lastReadings.length / 2)/1.1) * 60
+
+    //PID CONTROLLER
+    if(!inputs.raw) {
+        RpmController.setTarget(inputs.targetRPM);
+        inputs.firingDuration = Math.round(RpmController.update(outputs.rpm));
+
+        if(inputs.firingDuration > inputs.firingDurationMax) {
+            inputs.firingDuration = inputs.firingDurationMax;
+        } else if(inputs.firingDuration < inputs.firingDurationMin) {
+            inputs.firingDuration = inputs.firingDurationMin;
+        }
+
+        io.sockets.emit('INPUTS', inputs);
+    }
+
+    io.sockets.emit('OUTPUTS', outputs);
+}, 100);
+
+io.on('connection', (socket) => {
+    socketsConnected++;
+    logText('Socket ' + socket.id.substring(0, 10) + " connected. Connections: " + socketsConnected);
+
+    //SENDS UPDATE PACKET TO NEW CLIENT
+    socket.emit('INPUTS', inputs);
+
+    socket.on('SET', (data) => {
+        //IF CONTROL MODE CHANGED, RESET INPUTS AND PID CONTROLLER
+        if(data.raw !== inputs.raw) {
+
+            inputs.firingDelay = inputDefaults.firingDelay;
+            inputs.firingDuration = inputDefaults.firingDelay;
+            inputs.targetRPM = inputDefaults.targetRPM;
+
+            RpmController = resetRpmController();
+
+            inputs.raw = data.raw;
+        } else {
+            inputs = data;
+        }
+
+        io.sockets.emit('INPUTS', inputs);
+    });
+
+    socket.on('disconnect', () => {
+        socketsConnected--;
+        logText('Socket ' + socket.id.substring(0, 10) + " disconnected. Connections: " + socketsConnected);
+    });
+});
+
+// LOGS TEXT TO SERVER CONSOLE AND CLIENT
+function logText(text) {
+    let currentDate = new Date();
+    let timestamp = currentDate.getHours().toString().padStart(2, '0') + ':' + currentDate.getMinutes().toString().padStart(2, '0') + ':' + currentDate.getSeconds().toString().padStart(2, '0');
+
+    serverLog.push([timestamp , text])
+
+    io.sockets.emit('LOG', serverLog);
+    console.log(text);
+}
+
